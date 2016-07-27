b39a1040c0a483ea6451cea2563c9e1668452168
Jason Graham <Jason Graham>
2016-07-26 23:12:38 -0400

    Initial changes to include inspection data from Louisville Open Data.

---
+++ DangerouslyDelicious/DangerouslyDelicious.Tests/IntegrationTests/ServicesTests/RestaurantInspectionServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DangerouslyDelicious.Services;
+
+namespace DangerouslyDelicious.Tests.IntegrationTests.ServicesTests
+{
+    [TestClass]
+    public class RestaurantInspectionServiceTests
+    {
+        [TestMethod]
+        public void ItMustGetARestaurantInspectionDtoObjectByRestaurantName()
+        {
+            var name = "Royals Chicken";
+            var restaurantInspectionInfo = RestaurantInspectionService.GetRestaurantInspectionInfoByName(name);
+            Assert.IsNotNull(restaurantInspectionInfo);
+            Assert.IsTrue(name.Equals(restaurantInspectionInfo.EstablishmentName, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        [TestMethod]
+        public void ItMustReturnCleanJsonString()
+        {
+            var cleanJson = RestaurantInspectionService.CleanJsonString("([{\"EstablishmentID\":98995,\"InspectionID\":1171636,\"EstablishmentName\":\"ROYALS CHICKEN\",\"PlaceName\":\" \",\"Address\":\"736 E.MARKET\",\"Address2\":\"\",\"City\":\"LOUISVILLE\",\"State\":\"KY\",\"Zip\":\"40202\",\"TypeDescription\":\"FOOD SERVICE\",\"Latitude\":38.2531000,\"Longitude\":-85.7386000,\"InspectionDate\":\"2 / 10 / 2016\",\"Score\":100,\"Grade\":\"A\",\"NameSearch\":\"ROYALSCHICKEN\",\"Intersection\":null}] );");
+            Assert.AreEqual("{\"EstablishmentID\":98995,\"InspectionID\":1171636,\"EstablishmentName\":\"ROYALS CHICKEN\",\"PlaceName\":\" \",\"Address\":\"736 E.MARKET\",\"Address2\":\"\",\"City\":\"LOUISVILLE\",\"State\":\"KY\",\"Zip\":\"40202\",\"TypeDescription\":\"FOOD SERVICE\",\"Latitude\":38.2531000,\"Longitude\":-85.7386000,\"InspectionDate\":\"2 / 10 / 2016\",\"Score\":100,\"Grade\":\"A\",\"NameSearch\":\"ROYALSCHICKEN\",\"Intersection\":null}", cleanJson);
+        }
+    }
+}
--- DangerouslyDelicious/DangerouslyDelicious/Activities/RatingsViolationsComparisonActivity.cs
+++ DangerouslyDelicious/DangerouslyDelicious/Activities/RatingsViolationsComparisonActivity.cs
@@ -3,6 +3,7 @@ using Android.Content;
 using Android.OS;
 using Android.Widget;
 using DangerouslyDelicious.Dtos;
+using DangerouslyDelicious.Services;
 using DangerouslyDelicious.Utilities;
 using Newtonsoft.Json;
 
@@ -34,6 +35,32 @@ namespace DangerouslyDelicious.Activities
 
             var yelpRatings = FindViewById<TextView>(Resource.Id.comparisonNumberReviews);
             yelpRatings.Text = $"{restaurant.NumberReviews} Ratings";
+
+            #region Inspection Data
+            // TODO: find a way to include "inspectionData" as a Bundle attached to the Intent that kicked off this activity
+            // I could not quickly determine a way to convert my Dto object into a bundle. That's why this code is here.
+
+            var inspectionData = RestaurantInspectionService.GetRestaurantInspectionInfoByName(restaurant.Name);
+
+            if (!string.IsNullOrWhiteSpace(inspectionData.Grade))
+            {
+                var inspectionGrade = FindViewById<TextView>(Resource.Id.inspectionGrade);
+                inspectionGrade.Text = $"Grade: {inspectionData.Grade}";
+            }
+
+            if (inspectionData.Score != 0)
+            {
+                var inspectionScore = FindViewById<TextView>(Resource.Id.inspectionScore);
+                inspectionScore.Text = $"Score: {inspectionData.Score}";
+            }
+
+            if (!string.IsNullOrWhiteSpace(inspectionData.InspectionDate))
+            {
+                var inspectionDate = FindViewById<TextView>(Resource.Id.inspectionDate);
+                inspectionDate.Text = $"Inspection Date: {inspectionData.InspectionDate}";
+            }
+
+            #endregion Inspection Data
 
             var readReviewsButton = FindViewById<Button>(Resource.Id.readReviewsButton);
 
