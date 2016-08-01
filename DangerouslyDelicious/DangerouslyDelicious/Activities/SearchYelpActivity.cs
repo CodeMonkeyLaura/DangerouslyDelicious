e2d255220ff56846c36f8b192aec8f7e864266f8
Laura Williams <lwilliams@composablesystems.net>
2016-08-01 17:10:58 -0400

    No longer hangs with no internet access or location services not on.

--- DangerouslyDelicious/DangerouslyDelicious/Activities/SearchYelpActivity.cs
+++ DangerouslyDelicious/DangerouslyDelicious/Activities/SearchYelpActivity.cs
@@ -1,7 +1,9 @@
 using Android.App;
 using Android.Content;
+using Android.Net;
 using Android.OS;
 using Android.Widget;
+using DangerouslyDelicious.Models;
 using DangerouslyDelicious.Services;
 using DangerouslyDelicious.Utilities;
 
@@ -10,6 +12,8 @@ namespace DangerouslyDelicious.Activities
     [Activity(Theme = "@android:style/Theme.NoTitleBar")]
     public class SearchYelpActivity : Activity
     {
+        private ConnectivityManager _connectivityManager;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -22,21 +26,55 @@ namespace DangerouslyDelicious.Activities
 
             searchYelpButton.Click += async (sender, e) =>
             {
-                var searchString = @"https://api.yelp.com/v2/search?term=" + InputStringFormatting.RemoveUnsafeCharacters(restaurantSearchBox.Text) +
-                       @"&location=Louisville, KY";
+                var reqs = CheckInternetRequirements();
 
-                var restaurantList = await YelpData.PerformSearch(searchString);
+                if (reqs.Internet)
+                {
+                    var searchString = @"https://api.yelp.com/v2/search?term=" +
+                                       InputStringFormatting.RemoveUnsafeCharacters(restaurantSearchBox.Text) +
+                                       @"&location=Louisville, KY";
 
-                var intent = new Intent(this, typeof(YelpSearchResultActivity));
-                intent.PutExtra("restaurantList", (string)restaurantList);
-                intent.PutExtra("searchResultHeader", "Search Results");
-                StartActivity(intent);
+                    var restaurantList = await YelpData.PerformSearch(searchString);
+
+                    var intent = new Intent(this, typeof(YelpSearchResultActivity));
+                    intent.PutExtra("restaurantList", (string) restaurantList);
+                    intent.PutExtra("searchResultHeader", "Search Results");
+                    StartActivity(intent);
+                }
+                else
+                {
+                    ShowAlert("Internet access must be enabled to use this feature.");
+                }
             };
 
             backHomeButton.Click += (sender, e) =>
             {
                 StartActivity(typeof(MainActivity));
             };
+        }
+
+        private PhoneRequirements CheckInternetRequirements()
+        {
+            _connectivityManager = (ConnectivityManager)GetSystemService(ConnectivityService);
+
+            var internetAccess = _connectivityManager.ActiveNetworkInfo;
+
+            return new PhoneRequirements()
+            {
+                Internet = internetAccess != null && internetAccess.IsConnected
+            };
+        }
+
+        private void ShowAlert(string message)
+        {
+            var alert = new AlertDialog.Builder(this);
+            alert.Create();
+            alert.SetMessage(message);
+            alert.SetPositiveButton("Oops", delegate
+            {
+                StartActivity(typeof(MainActivity));
+            });
+            alert.Show();
         }
 
     }
---
+++ DangerouslyDelicious/DangerouslyDelicious/Models/PhoneRequirements.cs
@@ -0,0 +1,9 @@
+namespace DangerouslyDelicious.Models
+{
+    public class PhoneRequirements
+    {
+        public bool Network { get; set; }
+        public bool Gps { get; set; }
+        public bool Internet { get; set; }
+    }
+}
